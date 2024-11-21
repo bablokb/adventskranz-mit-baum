1715d31a68b37ccf2513df2b163cea0098a4af67
Bernhard Bablok <bablokb@gmx.de>
2024-11-21 16:11:02 +0100

    support second button to change brightness

--- src_python_btn/main.py
+++ src_python_btn/main.py
@@ -18,11 +18,13 @@ import neopixel
 import random
 import adafruit_fancyled.adafruit_fancyled as fancy
 
+LEVELS = [round(0.1*i,1) for i in range(0,11)]
+
 # values for Pimoroni Plasma2040
 PIN_NEO = board.DATA
 NUM_NEO = 4
 ORD_NEO = neopixel.RGB
-LVL_NEO = 0.4             # brightness
+LVL_NEO = 4               # index into brightness-levels
 INT_NEO = 5               # update interval for colors
 PIN_BTN = board.SW_A
 PIN_LVL = board.SW_B
@@ -30,12 +32,24 @@ INT_BTN = 0               # button delay interval (debounce)
 LED     = board.LED_R     # board led
 LED_ON  = False           # active low LED
 
+# values for Pico
+# PIN_NEO = board.GP18
+# NUM_NEO = 4
+# ORD_NEO = neopixel.GRB
+# LVL_NEO = 2               # index into brightness-levels
+# INT_NEO = 1               # update interval for colors
+# PIN_BTN = board.GP14
+# PIN_LVL = board.GP15
+# INT_BTN = 0.5             # button delay interval (debounce)
+# LED     = board.LED       # board led
+# LED_ON  = True            # active high LED
 
 # --- objects   -------------------------------------------------------------
 
 # list of neo-pixel-objects
 pixels = neopixel.NeoPixel(
-  PIN_NEO, NUM_NEO, brightness=LVL_NEO, auto_write=False, pixel_order=ORD_NEO)
+  PIN_NEO, NUM_NEO, brightness=LEVELS[LVL_NEO],
+  auto_write=False, pixel_order=ORD_NEO)
 time.sleep(1)
 
 # button
@@ -75,27 +89,43 @@ def wheel(pos):
 # --- main loop   -----------------------------------------------------------
 
 counter = 0
-col_pos = random.randrange(256)
+
+brit_index  = LVL_NEO
+col_pos     = random.randrange(256)
+color       = wheel(col_pos)
+color_adj   = fancy.gamma_adjust(fancy.CRGB(*color),gamma_value=2.2,
+                                 brightness=LEVELS[brit_index]).pack()
 last_btn    = 0
 last_update = 0
-brightness  = LVL_NEO
 
 while True:
-  # update counter
+  # update counter on button press
   if not btn.value and time.monotonic() > last_btn + INT_BTN:
     last_btn = time.monotonic()
     counter = (counter+1)%(NUM_NEO+1)
     led.value = LED_ON
     time.sleep(0.2)
     led.value = not LED_ON
+
+  # update brightness-level on button press
+  if not lvl.value and time.monotonic() > last_btn + INT_BTN:
+    last_btn = time.monotonic()
+    brit_index = 1 + brit_index%10
+    pixels.brightness = LEVELS[brit_index]
+    color_adj = fancy.gamma_adjust(fancy.CRGB(*color),gamma_value=2.2,
+                                   brightness=LEVELS[brit_index]).pack()
+    led.value = LED_ON
+    time.sleep(0.2)
+    led.value = not LED_ON
+
+  # update color after INT_NEO seconds
   if time.monotonic() > last_update + INT_NEO:
     # update color
     last_update = time.monotonic()
     col_pos     = (col_pos+1)%256
     color       = wheel(col_pos)
     color_adj = fancy.gamma_adjust(fancy.CRGB(*color),gamma_value=2.2,
-                                   brightness=brightness).pack()
-
+                                   brightness=LEVELS[brit_index]).pack()
   # update pixels
   for i in range(NUM_NEO):
     pixels[i] = color_adj if i<counter else (0,0,0)
